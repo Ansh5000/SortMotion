b61d4ccdf5a68aa4e3c5d0f5b1ca8f791a089f5b
Ansh soni <sonianshcsit@gmail.com>
2026-01-28 18:43:58 +0530

    feat: create basic skeletal structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,26 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="flex items-center justify-center">
+      <div className="flex flex-col gap-4 h-screen w-[90vw] bg-gray-50 p-4">
+        <header className="flex flex-col items-center justify-center gap-5 p-5 bg-blue-50 border border-gray-300 rounded-lg">
+          <div className="flex items-center justify-evenly gap-5">
+            <button></button>
+            <button></button>
+          </div>
+          <div className="flex items-center justify-evenly gap-5">
+            <button></button>
+            <div className="flex items-center justify-center gap-3">
+              <button></button>
+              <button></button>
+              <button></button>
+              <button></button>
+            </div>
+          </div>
+        </header>
+        <main className="bg-gray-200 border border-gray-400 shadow-sm rounded-t-lg rounded-b-sm w-full h-full"></main>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </div>
+  );
 }
 
-export default App
+export default App;
