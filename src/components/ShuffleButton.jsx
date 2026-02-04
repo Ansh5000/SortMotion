10d452023fbfa3497071d5d8d3f6b0b68d9ce93b
Ansh soni <sonianshcsit@gmail.com>
2026-02-04 15:19:17 +0530

    feat: add icon to shuffle button

--- src/components/ShuffleButton.jsx
+++ src/components/ShuffleButton.jsx
@@ -1,4 +1,5 @@
 import shuffleArray from "../utils/shuffleArray";
+import ShuffleIcon from "./icons/ShuffleIcon";
 
 const ShuffleButton = ({ array, setArray, isRunning }) => {
   return (
@@ -7,9 +8,10 @@ const ShuffleButton = ({ array, setArray, isRunning }) => {
         shuffleArray(array, setArray);
       }}
       disabled={isRunning}
-      className={`border-2 border-black px-5 py-0.5 rounded-xl bg-blue-500 text-white text-2xl font-semibold ${isRunning ? "cursor-not-allowed opacity-50" : "cursor-pointer opacity-100"}`}
+      className={`flex items-center justify-center gap-3 border-2 border-black px-5 py-0.5 rounded-xl bg-blue-500 ${isRunning ? "cursor-not-allowed opacity-50" : "cursor-pointer opacity-100"}`}
     >
-      Shuffle
+      <ShuffleIcon className="text-white" />
+      <p className="text-white text-2xl font-semibold">Shuffle</p>
     </button>
   );
 };
