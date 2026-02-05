86d0ed9c92a194612e520198a568521c9c89cd76
Ansh soni <sonianshcsit@gmail.com>
2026-02-05 11:11:55 +0530

    fix: add another option of pivot to indicate pivot in case of quick sort

--- src/components/Bar.jsx
+++ src/components/Bar.jsx
@@ -1,8 +1,9 @@
-const Bar = ({ value, outer, inner, index }) => {
+const Bar = ({ value, outer, inner, pivot, index }) => {
   const getBgColor = () => {
     if (outer === index && inner === index) return "bg-purple-500";
     if (outer === index) return "bg-sky-500";
     if (inner === index) return "bg-yellow-400";
+    if (pivot === index) return "bg-pink-500";
     return "bg-gray-950";
   };
   return (
