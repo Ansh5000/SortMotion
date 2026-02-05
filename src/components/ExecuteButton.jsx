d0aacc67ec402a04327775b44a996e93d49f1f63
Ansh soni <sonianshcsit@gmail.com>
2026-02-05 11:14:14 +0530

    refactor: removing trailing whitespace

--- src/components/ExecuteButton.jsx
+++ src/components/ExecuteButton.jsx
@@ -3,7 +3,7 @@ const ExecuteButton = ({ onclick }) => {
   return (
     <button
       onClick={onclick}
-      className="flex items-center justify-center gap-3 cursor-pointer border-2 border-black px-5 py-0.5 rounded-xl bg-orange-500 "
+      className="flex items-center justify-center gap-3 cursor-pointer border-2 border-black px-5 py-0.5 rounded-xl bg-orange-500"
     >
       <PlayIcon className="text-white" />
       <p className="text-white text-2xl font-semibold">Start</p>
